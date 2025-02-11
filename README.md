7c9af135828a5593cb4078391042680aaa88d66e
Shivam Sharma <meshivam81@gmail.com>
2025-02-11 18:43:18 +0530

    feat: psql connection

---
+++ README.md
@@ -0,0 +1,35 @@
+## product-store
+
+Highlights:
+
+- 🌟 Tech stack: PERN + TailwindCSS + Daisy UI
+- 🚀 Rate Limiting & Bot Detection
+- 👌 Global state management with Zustand
+- 🐞 Error handling both on the server and client
+
+### Setup .env file
+
+```js
+PORT=3000
+
+PGUSER=...
+PGPASSWORD=...
+PGHOST=...
+PGDATABASE=...
+
+ARCJET_KEY=...
+ARCJET_ENV=development
+```
+
+### Run the API
+
+```bash
+npm run dev
+```
+
+### Run the frontend
+
+```bash
+cd frontend
+npm run dev
+```
---
+++ server/server.js
@@ -0,0 +1,44 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+import dotenv from "dotenv";
+
+import productRoutes from "./routes/productRoute.js";
+import { sql } from "./db/db.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(morgan("dev"));
+
+app.use("/api/products", productRoutes);
+
+async function initDB() {
+  try {
+    await sql`
+        CREATE TABLE IF NOT EXISTS products (
+          id SERIAL PRIMARY KEY,
+          name VARCHAR(255) NOT NULL,
+          image VARCHAR(255) NOT NULL,
+          price DECIMAL(10, 2) NOT NULL,
+          created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )
+      `;
+
+    console.log("Database initialized successfully");
+  } catch (error) {
+    console.log("Error initDB", error);
+  }
+}
+
+initDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`);
+  });
+});
