4b733a7c7376bb9e00c1457f0de4640ab05f04a0
Shivam Sharma <meshivam81@gmail.com>
2025-02-18 09:42:57 +0530

    feat(ui): add navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,11 +2,14 @@ import { Routes, Route } from "react-router-dom";
 import { Navbar } from "./components/Navbar";
 
 import HomePage from "./pages/HomePage";
-import ProductPage from "./pages/ProductPage"
+import ProductPage from "./pages/ProductPage";
 
 function App() {
   return (
-    <div className="min-h-screen bg-base-200 transition-colors duration-300">
+    <div
+      className="min-h-screen bg-base-200 transition-colors duration-300"
+      data-theme="forest"
+    >
       <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,5 +1,28 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { ShoppingCartIcon } from "lucide-react";
 
 export const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="bg-base-100/80 backdrop-blur-lg border-b border-base-content/10 sticky top-0 z-50">
+      <div className="max-w-7xl mx-auto">
+        <div className="navbar min-h-[4rem] justify-between">
+          <div className="flex-1 lg:flex-none">
+            <Link to="/" className="hover:opacity-80 transition-opacity">
+              <div className="flex items-center gap-2">
+                <ShoppingCartIcon className="size-9 text-primary" />
+                <span
+                  className="font-semibold font-mono tracking-widest text-2xl
+               bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary"
+                >
+                  PODUCTSTORE
+                </span>
+              </div>
+            </Link>
+          </div>
+          {/*Todo: RIGHT ELEMENTS */}
+        </div>
+      </div>
+    </div>
+  );
 };
