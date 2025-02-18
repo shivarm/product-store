8178bc23c3f3682182a7667bb1c13b2b10bd3fa1
Shivam Sharma <meshivam81@gmail.com>
2025-02-18 14:22:37 +0530

    feat(ui): add theme selector

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,12 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { ShoppingCartIcon } from "lucide-react";
+import { Link, useResolvedPath } from "react-router-dom";
+import { ShoppingBagIcon, ShoppingCartIcon } from "lucide-react";
+import { ThemeSelector } from "./ThemeSelector";
 
 export const Navbar = () => {
+  const { pathname } = useResolvedPath();
+  const isHomePage = pathname === "/";
+
   return (
     <div className="bg-base-100/80 backdrop-blur-lg border-b border-base-content/10 sticky top-0 z-50">
       <div className="max-w-7xl mx-auto">
@@ -20,7 +24,20 @@ export const Navbar = () => {
               </div>
             </Link>
           </div>
-          {/*Todo: RIGHT ELEMENTS */}
+          <div className="flex items-center gap-4">
+            <ThemeSelector />
+
+            {isHomePage && (
+              <div className="indicator">
+                <div className="p-2 rounded-full hover:base-200 transition-colors">
+                  <ShoppingBagIcon className="size-5" />
+                  <span className="badge badge-sm badge-primary indicator-item">
+                    8
+                  </span>
+                </div>
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </div>
